0f517ee4585dc05e02942f37c5df28016d025322
iot-liujunyang <liujunyang@haier.com>
2022-04-26 17:15:03 +0800

    add trap rain problem

--- 31.js
+++ 31.js
@@ -4279,6 +4279,7 @@ funMap.minCost2 = () => {
     let dp = [...costs[0]]
     let temp = []
 
+    // 这里还可以仿照小册，直接改变输入的数组
     for (let i = 1; i < len; i++) {
       for (let j = 0; j < 3; j++) {
         let { a, b } = getIndex(j)
@@ -4295,4 +4296,29 @@ funMap.minCost2 = () => {
   console.log(minCost2(costs))
 }
 
-funMap.minCost2()
+// funMap.minCost2()
+
+// 头条真题
+/**
+ * 接雨水问题
+ *
+ * 题目描述：给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
+ *
+ * 上面是由数组 [0,1,0,2,1,0,1,3,2,1,2,1] 表示的高度图，在这种情况下，可以接 6 个单位的雨水（蓝色部分表示雨水）。
+ *
+ * 输入: [0,1,0,2,1,0,1,3,2,1,2,1]
+ * 输出: 6
+ *
+ * 命题关键字：双指针法、数组、模拟
+ *
+ * ============================================
+ * 两侧边缘外的会流走，只有柱子之间能存水
+ *
+ */
+funMap.trapRain = () => {
+  function trapRain() {}
+
+  console.log(trapRain())
+}
+
+funMap.trapRain()
